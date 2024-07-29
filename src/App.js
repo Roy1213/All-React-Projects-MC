a67438d44f2eeba72a46590746ecfede89406fc3
Roy Alameh <royalameh1213@gmail.com>
2024-07-28 20:13:39 -0500

    Added animations and scrolling, multiple unique rows with tag generation function.

--- src/App.js
+++ src/App.js
@@ -4,17 +4,32 @@ import Footer from './Footer'
 import Remote from './Remote';
 
 function App() {
-
+  const generateEmptySpace = (num) => {
+    const rows = []
+    for (let i = 0; i < num; i++) {
+      rows.push(<br key={i} />)
+    }
+    return <div>{rows}</div>
+  }
   return (
     <div className="App" style={{
-      //backgroundColor: `rgb(0, 0, 0)`,
+      backgroundColor: `rgb(0, 0, 0)`,
+      minHeight: '100vh',
+      //display: 'flex',
+     //alignItems: 'center',
+      //justifyContent: 'center'
       // alignItems: 'center',
       // height: '100%'
     }}>
       {/*<Content></Content>*/}
       {/* can also use self-closing tags */}
       {/* <Footer /> */}
+
+      {generateEmptySpace(5)}
       <Remote/>
+
+
+
     </div>
   );
 }
--- src/Remote.js
+++ src/Remote.js
@@ -1,6 +1,6 @@
-import Open from "./Open";
-import OpenContent from "./OpenContent";
-import Closed from "./Closed";
+import Open from './Open';
+import OpenContent from './OpenContent';
+import Closed from './Closed';
 import { useState } from 'react';
 
 const Remote = () => {
@@ -8,25 +8,128 @@ const Remote = () => {
     const handleClick = () => {
         console.log('Clicked')
         setOpen(!isOpen)
-    } 
+    }
+    const click = () => {
+        console.log('You clicked it')
+    }
+    const generateContent = () => {
+        const text1 = [
+            "Dryer State:",
+            "Disch Mode:",
+            "Disch Speed SP:",
+            "Mid-Grain SP:",
+            "Disch Moist SP:",
+            "Top Plenum(sp):",
+            "Bot Plenum(sp):"
+        ]
+        const text2 = [
+            "Running",
+            "Manual",
+            "10 %",
+            "120 F",
+            "15.0 %",
+            "180 F",
+            "180 F"
+        ]
+        const rows = []
+        for (let i = 0; i < 7; i++) {
+            rows.push(
+                <div key={i} style={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    paddingLeft: 10,
+                    paddingRight: 10
+                }}>
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'center'
+                    }}>
+                        <p style={{
+                            paddingLeft: 5
+                        }}>{text1[i]}</p>
+                        <p style={{
+                            paddingLeft: 5,
+                            color: 'lime'
+                        }}>{text2[i]}</p>
+                    </div>
+
+                    <button onClick={click} style={{
+                        backgroundColor: 'rgb(134, 38, 51)',
+                        borderWidth: 1,
+                        color: 'white',
+                        maxHeight: 40,
+                        borderRadius: 10
+                    }}>Change...</button>
+                </div>)
+        }
+        return <div>{rows}</div>
+    }
     return (
-        <div>
+        <div style={{
+            alignItems: 'center'
+        }}>
             <button onClick={handleClick} style={{
                 backgroundColor: `rgb(35, 35, 35)`,
                 borderColor: `rgb(35, 35, 35)`,
                 minWidth: '50%',
+                maxWidth: '50%',
+                overflowX: 'hidden',
+                paddingLeft: 30,
+                paddingRight: 30,
+                maxHeight: 50,
                 border: 0,
-                ...(isOpen ? {borderTopLeftRadius: '10px', borderTopRightRadius: '10px', } : {borderRadius: '10px'})
+                overflow: 'hidden',
+                ...(isOpen ? { borderTopLeftRadius: '10px', borderTopRightRadius: '10px' } : { borderTopLeftRadius: '10px', borderTopRightRadius: '10px', })
             }}>
-                <div>
-                    {isOpen ? (<Open/>) : (<Closed/>)}
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                }}>
+                    <h1 style={{ color: 'white' }}>Remote Control</h1>
+                    <h1 style={{ color: `rgb(35, 35, 35)` }}>_</h1>
+                    {isOpen ? <h1 style={{ color: 'white' }}>⊖</h1> : <h1 style={{ color: 'white' }}>⊕</h1>}
                 </div>
             </button>
-            <div>
-                {isOpen ? (<OpenContent/>) : (<></>)}
+            <div style={{
+                color: 'white',
+                backgroundColor: `rgb(35, 35, 35)`,
+                maxWidth: '50%',
+                minWidth: '50%',
+                margin: '0 auto',
+                padding: 0,
+                //paddingBottom: 10,
+                //opacity: isOpen ? '1' : '0',
+                //vh or vw
+                maxHeight: isOpen ? 365 : 0,
+                transition: isOpen ? 'all ease-out .5' : 'all ease-in .5s',
+                overflowY: 'auto',
+                overflowX: 'hidden',
+                scrollbarColor: `rgb(134, 38, 51) rgb(35, 35, 35)`
+                //visibility: isOpen ? 'visible' : 'hidden'
+            }}>
+
+                {/*<a href='https://www.google.com/' style={{ color: 'white' }}>Click this link</a>*/}
+                <br/>
+                {generateContent()}
+                <p style={{
+                    color: 'yellow',
+                    textAlign: 'left',
+                    paddingLeft: 15
+                }}>
+                    NOTE: This reflects the current status of the dryer and may not match values displayed elsewhere in M-C Trax (data on M-C Trax is updated from the dryer every 60 seconds).
+                </p>
             </div>
-            
-            
+            <div style={{
+                borderBottomLeftRadius: '10px',
+                borderBottomRightRadius: '10px',
+                backgroundColor: `rgb(35, 35, 35)`,
+                borderColor: `rgb(35, 35, 35)`,
+                minWidth: '50%',
+                maxWidth: '50%',
+                margin: '0 auto'
+            }}>
+                <br></br>
+            </div>
         </div>
     )
 }
