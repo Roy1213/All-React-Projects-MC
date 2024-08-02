0a956ca0ba94c72efbe5577f83bc160262ee4945
Roy Alameh <royalameh1213@gmail.com>
2024-08-02 11:18:32 -0500

    Worked on grid layout

--- src/App.js
+++ src/App.js
@@ -1,38 +1,42 @@
-import './App.css';
-import Content from './Content'
-import Footer from './Footer'
-import Remote from './Remote';
+// import './App.css';
+// import Content from './Content'
+// import Footer from './Footer'
+// import Remote from './Remote';
+// import { GridView } from '@mui/icons-material';
 
 
-function App() {
-  const generateEmptySpace = (num) => {
-    const rows = []
-    for (let i = 0; i < num; i++) {
-      rows.push(<br key={i} />)
-    }
-    return <div>{rows}</div>
-  }
-  return (
-    <div className="App" style={{
-      backgroundColor: `rgb(0, 0, 0)`,
-      minHeight: '100vh',
-      //display: 'flex',
-     //alignItems: 'center',
-      //justifyContent: 'center'
-      // alignItems: 'center',
-      // height: '100%'
-    }}>
-      {/*<Content></Content>*/}
-      {/* can also use self-closing tags */}
-      {/* <Footer /> */}
+// function App() {
+//   const generateEmptySpace = (num) => {
+//     const rows = []
+//     for (let i = 0; i < num; i++) {
+//       rows.push(<br key={i} />)
+//     }
+//     return <div>{rows}</div>
+//   }
+//   return (
+//     <div className="App" style={{
 
-      {generateEmptySpace(2)}
-      <Remote/>
+      
+//       //backgroundColor: `rgb(0, 0, 0)`,
+//       //minHeight: '100vh',
 
 
+//       //display: 'flex',
+//      //alignItems: 'center',
+//       //justifyContent: 'center'
+//       // alignItems: 'center',
+//       // height: '100%'
+//     }}>
+//       {/*<Content></Content>*/}
+//       {/* can also use self-closing tags */}
+//       {/* <Footer /> */}
 
-    </div>
-  );
-}
+//       {/* {generateEmptySpace(2)}
+//       <Remote/> */}
+//       <GridView/>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
+
